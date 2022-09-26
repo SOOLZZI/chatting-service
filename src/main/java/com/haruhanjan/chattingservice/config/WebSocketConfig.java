f565d5a5e0ab10febee6788555deb6f39456bb8b
SEONMl <sm000117@naver.com>
2022-09-26 22:49:05 +0900

    fix: WebSocketConfig 수정

--- src/main/java/com/haruhanjan/chattingservice/config/WebSocketConfig.java
+++ src/main/java/com/haruhanjan/chattingservice/config/WebSocketConfig.java
@@ -1,36 +1,32 @@
 package com.haruhanjan.chattingservice.config;
 
-import com.haruhanjan.chattingservice.controller.ChatHandler;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Profile("stomp")
 @Configuration
 @EnableWebSocketMessageBroker
-@RequiredArgsConstructor
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final ChatHandler chatHandler;
 
     @Value("${front.base_url}")
     private String frontURL;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // handshaking 담당
-        registry.addEndpoint("/ws/chat").setAllowedOrigins(frontURL).withSockJS();
+        // handshaking 담당                         CORS 설정 ─┐
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*");//.withSockJS();
+        registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic","/queue"); // 1:N, 1:1
-        registry.setApplicationDestinationPrefixes("/app"); // client의 SEND 요청 처리
+        registry.enableSimpleBroker("/sub"); // 1:N, 1:1
+        registry.setApplicationDestinationPrefixes("/pub"); // client의 SEND 요청 처리
     }
 
 }
